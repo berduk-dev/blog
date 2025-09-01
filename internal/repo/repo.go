cb5ee954abbfaa21e948a17b9b3abda371de4c79
Win-10 <Win-10@MacBook-Pro.local>
2025-09-01 22:09:33 +0300

    Добавил некоторые функции Get и Create в repo.go

---
+++ internal/repo/repo.go
@@ -0,0 +1,141 @@
+package repo
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"time"
+)
+
+type Repository struct {
+	db *pgx.Conn
+}
+
+func New(db *pgx.Conn) Repository {
+	return Repository{
+		db: db,
+	}
+}
+
+type User struct {
+	userID    int
+	name      string
+	email     string
+	isAdmin   bool
+	createdAt time.Time
+	updatedAt time.Time
+}
+
+type CreateUserRequest struct {
+	name  string
+	email string
+}
+
+type Post struct {
+	postID    int
+	userID    int
+	title     string
+	body      string
+	views     int
+	createdAt time.Time
+	updatedAt time.Time
+}
+
+type CreatePostRequest struct {
+	userID int
+	title  string
+	body   string
+}
+
+type Comment struct {
+	commentID int
+	postID    int
+	userID    int
+	body      string
+	createdAt time.Time
+	updatedAt time.Time
+}
+
+type CreateCommentRequest struct {
+	postID int
+	userID int
+	body   string
+}
+
+func (r *Repository) GetUserByID(ctx context.Context, userID int) (*User, error) {
+	var user User
+
+	rows, err := r.db.Query(ctx, "SELECT name, email, is_admin, created_at, updated_at FROM users WHERE user_id = $1", userID)
+	if err != nil {
+		return nil, fmt.Errorf("error User r.db.Query: %w", err)
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&user.name, &user.email, &user.isAdmin, &user.createdAt, &user.updatedAt)
+		if err != nil {
+			return nil, fmt.Errorf("error User Scan: %w", err)
+		}
+	}
+	return &user, nil
+}
+
+func (r *Repository) GetPostByID(ctx context.Context, postID int) (*Post, error) {
+	var post Post
+
+	rows, err := r.db.Query(ctx, "SELECT user_id, title, body, views, created_at, updated_at FROM posts WHERE user_id = $1", postID)
+	if err != nil {
+		return nil, fmt.Errorf("error User r.db.Query: %w", err)
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&post.userID, &post.title, &post.body, &post.views, &post.createdAt, &post.updatedAt)
+		if err != nil {
+			return nil, fmt.Errorf("error User Scan: %w", err)
+		}
+	}
+	return &post, nil
+}
+
+func (r *Repository) GetCommentByID(ctx context.Context, commentID int) (*Comment, error) {
+	var comment Comment
+
+	rows, err := r.db.Query(ctx, "SELECT user_id, post_id, body, created_at, updated_at FROM posts WHERE user_id = $1", commentID)
+	if err != nil {
+		return nil, fmt.Errorf("error User r.db.Query: %w", err)
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&comment.userID, &comment.postID, &comment.body, &comment.createdAt, &comment.updatedAt)
+		if err != nil {
+			return nil, fmt.Errorf("error User Scan: %w", err)
+		}
+	}
+	return &comment, nil
+}
+
+func (r *Repository) CreateUser(ctx context.Context, params CreateUserRequest) error {
+	_, err := r.db.Exec(ctx, "INSERT INTO users (name, email) VALUES ($1, $2)", params.name, params.email)
+	if err != nil {
+		return fmt.Errorf("error CreateUser Exec: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repository) CreatePost(ctx context.Context, params CreatePostRequest) error {
+	_, err := r.db.Exec(ctx, "INSERT INTO posts (user_id, title, body) VALUES ($1, $2)", params.userID, params.title, params.body)
+	if err != nil {
+		return fmt.Errorf("error CreatePost Exec: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repository) CreateComment(ctx context.Context, params CreateCommentRequest) error {
+	_, err := r.db.Exec(ctx, "INSERT INTO posts (user_id, title, body) VALUES ($1, $2)", params.userID, params.postID, params.body)
+	if err != nil {
+		return fmt.Errorf("error CreateComment Exec: %w", err)
+	}
+
+	return nil
+}
---
+++ migrations/20250901111339_init.sql
@@ -0,0 +1,35 @@
+-- +goose Up
+-- +goose StatementBegin
+create table posts (
+    id bigserial primary key,
+    user_id bigint not null,
+    title text not null,
+    body text not null,
+    views integer not null default 0,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now()
+);
+
+create table users (
+    id bigserial primary key,
+    name text not null,
+    email text not null,
+    is_admin boolean not null default false,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now()
+);
+
+create table comments (
+    id bigserial primary key,
+    user_id bigint not null,
+    post_id bigint not null,
+    body text not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now()
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+-- +goose StatementEnd
